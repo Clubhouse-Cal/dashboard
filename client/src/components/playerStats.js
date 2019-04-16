ff24b425f830c7a058731447f800089dc3e6e3c3
mluong888 <mluong888@berkeley.edu>
2019-04-15 19:58:40 -0700

    players have their respective photos

--- client/src/components/playerStats.js
+++ client/src/components/playerStats.js
@@ -4,7 +4,8 @@ export class playerStats extends Component {
   render() {
       //console.log(this.props.players);
       var number = this.props.players.map(d => d[0]);
-      var name = this.props.players.map(d => d[2] + " " + d[1]);
+      var firstName = this.props.players.map(d => d[2]);
+      var lastName = this.props.players.map(d => d[1]);
       var position = this.props.players.map(d => d[3]);
       var height = this.props.players.map(d => d[4]);
       var weight = this.props.players.map(d => d[5]);
@@ -21,20 +22,20 @@ export class playerStats extends Component {
             </div>
             <div className = "playerBio">
                 {/* insert image here  */}
-                <img src = {"132.png"}></img>
+                <img src = {require("../pictures/photos/"+firstName[0].toLowerCase()+lastName[0].toLowerCase()+".jpg")} alt="helllo"></img>
                 <div className = "bioInfo">
-                    <p> {number[0]} {name[0]}</p>
+                    <p> {number[0]} {firstName[0] + " " +lastName[0]}</p>
                     <p> Position: {position[0]}</p>
                     <p> Height: {height[0]}</p>
                     <p> Weight: {weight[0]} </p>
                     <p> Class: {year[0]}</p>
                     <p> Hometown/ Last School: {hometown[0]}</p>
-                
+
                 </div>
 
             </div>
         </div>
-        
+
     )
   }
 }
--- client/src/components/playercard.js
+++ client/src/components/playercard.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import pic from '../pictures/sam.jpg';
+//import pic from '../pictures/sam.jpg';
 
 export class playercard extends Component {
   render() {
     var name = this.props.name;
-    console.log(name==null);
     if (name!=null){
       return (
-          
+
         <div className = "playerCard">
-          <img className = "portrait" src = {pic}/>
+          <img className = "portrait" src = {require("../pictures/photos/"+name[0].toLowerCase()+name[1].toLowerCase()+".jpg")}/>
           <p className = "playerName">{name[0] + " " + name[1]}</p>
         </div>
       );
     }
         return (
-          
-          // <div className = "playerCard">
-          //   <img className = "portrait" src = {pic}/>
-          //   <p className = "playerName">{name[0] + " " + name[1]}</p>
-          // </div>
           <div>
 
           </div>
