ac0c00ccaefb26924cce87df6c16821d4275ce0a
mluong888 <mluong888@berkeley.edu>
2019-04-22 16:11:52 -0700

    almost working from home to stats, no pic tho

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,32 +5,12 @@ import Dropdown from './dropdown';
 import TeamPreview from './teamPreview'
 
 export class Home extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     players: [],
-  //     playerFirstLastName: [],
-  //     leader: []
-  //   };
-  // }
-
-//   componentDidMount() {
-//     fetch('/api/players')
-//       .then(res => res.json())
-//       .then(data => {
-//         this.setState({
-//           players: data.data,
-//           playerFirstLastName: data.data.map(d => [d[2],d[1]]),
-//           leader: ["Arman", "Sabouri"],
-//         })
-//       });
-// }
 
 
   render() {
     var playerFirstLastName = this.props.players.map(d => [d[2], d[1]]);
+    var playerData = this.props.players.map(d => d);
     var leader = ["Arman", "Sabouri"];
-    console.log(playerFirstLastName);
     return (
       <div className = "page">
           <p className = "title">Home</p>
@@ -39,7 +19,7 @@ export class Home extends Component {
             <Dropdown/>
           </div>
           <div className = "playerScroll">
-            <PlayerScroll names={playerFirstLastName}/>
+            <PlayerScroll data = {playerData}/>
           </div>
 
           <p> Upcoming game on 10/14/2018</p>
