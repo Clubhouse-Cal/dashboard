67543c4cec410b2a54059b2b412597aa6a2d03ed
Dylan <ddreyer@berkeley.edu>
2019-04-22 22:04:08 -0700

    fix image error

--- client/src/components/playercard.js
+++ client/src/components/playercard.js
@@ -5,10 +5,11 @@ export class playercard extends Component {
   render() {
     var name = this.props.name;
     if (name!=null){
+      var imageSrc = '/images/photos/'+name[0].toLowerCase()+name[1].toLowerCase()+'.jpg'
       return (
 
         <div className = "playerCard">
-        <img className = "portrait" src = {require("../pictures/photos/"+name[0].toLowerCase()+name[1].toLowerCase()+".jpg")}/>
+        <img className = "portrait" src = {imageSrc}/>
         <p className = "playerName">{name[0] + " " + name[1]}</p>
 
         </div>
